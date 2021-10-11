c51373caadb485adf4459c11a8b5ad88b0dfcd3a
yesia <yesia.plorina@pgi-data.com>
2021-10-11 14:06:07 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={{display: 'flex', flex: 1, padding: 100}}>
+      <Text>Top Ratting</Text>
+    </View>
+  );
+};
+
+export default App;
